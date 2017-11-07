00afbb6528be006cb8c8546515cf63f5f3be2d6b
Jeremy Park <cyanic@gmail.com>
2017-11-07 11:09:45 +0900

    for homework

--- word2vec_basic_mycorpus.py
+++ word2vec_basic_mycorpus.py
@@ -43,7 +43,7 @@ vocabulary = read_data(filename)
 print('Data size', len(vocabulary))
 
 # Step 2: Build the dictionary and replace rare words with UNK token.
-vocabulary_size = 60000
+vocabulary_size = 100000
 
 def build_dataset(words, n_words):
   """Process raw inputs into a dataset."""
